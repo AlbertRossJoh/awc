05d94f88c8d2ef358bf598e2d31c6184e6e803c8
AlbertRossJoh <albertrossj@gmail.com>
2025-09-08 15:56:53 +0200

    Added comment to full flag function

--- awc/FlagParser/FlagHelpers.cs
+++ awc/FlagParser/FlagHelpers.cs
@@ -2,6 +2,12 @@ namespace awc.FlagParser;
 
 internal static class FlagHelpers
 {
+    /// <summary>
+    /// Converts a full lenght flag to its shorthand equivalent e.g. --flag turns into -f
+    /// </summary>
+    /// <param name="fullFlag">The full flag</param>
+    /// <returns>The shorthand version of the flag</returns>
+    /// <exception cref="ArgumentException">Thrown if the full flag does not meet the requirements, e.g., does not start with --</exception>
     public static string FullFlagToShortFlag(string fullFlag)
     {
         if (!fullFlag.StartsWith("--")) throw new ArgumentException($"Invalid flag: {fullFlag}, full flags should start with '--', e.g. --flag");
